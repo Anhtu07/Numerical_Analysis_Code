f44d00636ce9a9119502a2379e269ea3c2ce73de
Anhtu07 <tu.nguyenanh.96@gmail.com>
2016-09-05 15:13:49 +0700

    add

---
+++ daoham1.m
@@ -0,0 +1,28 @@
+function m = daoham1 (f, x)
+%input: f la ham so
+%       x la diem can dao ham
+%output:m la gia tri dao ham f tai x
+step = 0.01;
+prev = 0;
+l = (f(x)-f(x-step))/step;
+while abs(l-prev) > 1e-7,
+  prev = (f(x)-f(x-step))/step;
+  step = step/10;
+  l = (f(x)-f(x-step))/step;
+  end;
+
+prev = 0;  
+step = 0.01;
+r = (-f(x)+f(x+step))/step;
+while abs(r-prev) > 1e-7,
+  prev = (-f(x)+f(x+step))/step;
+  step = step/10;
+  r = (-f(x)+f(x+step))/step;
+  end;
+  
+if abs(l-r) < 1e-6,
+  m = l;
+else
+  m = null;
+  end;
+endfunction
---
+++ for_test.m
@@ -0,0 +1,4 @@
+function x = for_test (g)
+syms x;
+x = limit(g, x, 0)
+endfunction
